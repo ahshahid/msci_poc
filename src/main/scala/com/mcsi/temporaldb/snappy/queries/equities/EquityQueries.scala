44e0b8d41073b9818504b0610ed4a6667eaead9f
Asif Shahid <ashahid@snappydata.io>
2016-12-24 11:30:46 -0800

    added support for passing time parameter as just date

--- src/main/scala/com/mcsi/temporaldb/snappy/queries/equities/EquityQueries.scala
+++ src/main/scala/com/mcsi/temporaldb/snappy/queries/equities/EquityQueries.scala
@@ -20,6 +20,8 @@ object EquityQueries {
      new SimpleDateFormat("HH:mm a"), new SimpleDateFormat("HH:mm"),
     new SimpleDateFormat("HH:mm:ss"), new SimpleDateFormat("HH:mm:ss a"))
 
+  val q5_6JustDateFormatter =  new SimpleDateFormat("yyyy-mm-dd")
+
   val query1 = s"""select distinct last_value(obs_date) over win as observation_time,
                   last_value(val) over win  from
                  ${Constants.BTS_IR_OBS} as obs,  ${Constants.BRF_CON_INST} as instr where
@@ -32,18 +34,18 @@ object EquityQueries {
   val query2 = s"""select distinct last_value(obs_date) over win as observation_time,
                  last_value(val) over win  from  ${Constants.BTS_IR_OBS} as obs,
                  ${Constants.BRF_CON_INST} as instr where
-                 (hour(obs_date) * 3600 + minute(obs_date) *60 + second(obs_date)) < '%3$$s' and
+                 (hour(obs_date) * 3600 + minute(obs_date) *60 + second(obs_date)) <= '%3$$s' and
                  instr.Name = '%1$$s' and instr.ID = obs.NODE_ID
-                  and obs.val_type =  %2$$s  order by obs_date window win as
-                  ( partition by date_format(obs_date,'yyyy.MM.dd') order by observation_time ,
+                  and obs.val_type =  %2$$s  order by observation_time window win as
+                  ( partition by date_format(obs_date,'yyyy.MM.dd') order by obs_date ,
                   obs.TRANS_RNG_START  rows between unbounded preceding and unbounded following)"""
 
   val query3 = s"""select distinct last_value(obs_date) over win as observation_time,
                  last_value(val) over win  from
                  ${Constants.BTS_IR_OBS} as obs,  ${Constants.BRF_CON_INST} as instr where
-                 year(obs_date) <= %3$$s and year(obs_date) > (%3$$s -1) and and  instr.Name =
-                 '%1$$s' and instr.ID = obs.NODE_ID
-                  and obs.val_type =  %2$$s  order by observation_time window win as
+                 year(obs_date) = %3$$s  and  instr.Name = '%1$$s'
+                  and instr.ID = obs.NODE_ID  and obs.val_type =  %2$$s
+                  order by observation_time window win as
                   ( partition by date_format(obs_date,'yyyy.MM.dd') order by obs_date ,
                   obs.TRANS_RNG_START  rows between unbounded preceding and unbounded following)"""
 
@@ -59,8 +61,8 @@ object EquityQueries {
                  last_value(val) over win  from  ${Constants.BTS_IR_OBS} as obs,
                  ${Constants.BRF_CON_INST} as instr where
                  obs_date <= '%3$$s' and  instr.Name = '%1$$s' and instr.ID = obs.NODE_ID
-                  and obs.val_type =  %2$$s  order by obs_date window win as
-                  ( partition by date_format(obs_date,'yyyy.MM.dd') order by observation_time ,
+                  and obs.val_type =  %2$$s  order by observation_time window win as
+                  ( partition by date_format(obs_date,'yyyy.MM.dd') order by obs_date ,
                   obs.TRANS_RNG_START  rows between unbounded preceding and unbounded following)"""
 
   val query6 = s"""select distinct obs_date  as observation_time,
@@ -142,7 +144,7 @@ object EquityQueries {
     queryExecutor.executeQuery[(Timestamp,T)](query, queryExecutor.getTransformerForObservations[T])
   }
 
-  def getAllValue1AttribPerDayForYear[T: TypeTag, R](instrumentName: String,
+  def getAllValue1AttribPerDay[T: TypeTag, R](instrumentName: String,
   attributeName: String, queryExecutor: QueryExecutor[R]): Iterator[(Timestamp, T)]
   =  {
     val valType = AttributeCache.getValType(attributeName)
@@ -154,8 +156,32 @@ object EquityQueries {
   def get1Value1AttribPerDayLastTimestampTillDate[T: TypeTag, R](instrumentName: String,
          attributeName: String, tillDate: String, queryExecutor: QueryExecutor[R]):
   Iterator[(Timestamp, T)]  =  {
+    val ts = {
+      try {
+        Timestamp.valueOf(tillDate).getTime
+      }catch {
+        case e: Exception => q5_6JustDateFormatter.parse(tillDate).getTime
+      }
+    }
+
+    val cal = Calendar.getInstance()
+    cal.setTimeInMillis(ts)
+    val hr = cal.get(Calendar.HOUR_OF_DAY)
+    val min = cal.get(Calendar.MINUTE)
+    val sec = cal.get(Calendar.SECOND)
+    val modDate = if(hr == 0 && min == 1 && sec == 0) {
+      //add time till end of day
+      cal.add(Calendar.HOUR_OF_DAY, 23)
+      cal.add(Calendar.MINUTE, 58)
+      cal.add(Calendar.SECOND, 59)
+      cal.get(Calendar.YEAR) +"-" + (cal.get(Calendar.MONTH) + 1) + "-" + cal.get(Calendar
+        .DAY_OF_MONTH) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) +
+      ":" + cal.get(Calendar.SECOND)
+    }else {
+      tillDate
+    }
     val valType = AttributeCache.getValType(attributeName)
-    val query = query5.format(instrumentName, valType, tillDate)
+    val query = query5.format(instrumentName, valType, modDate)
     println("equity query5 = " + query)
     queryExecutor.executeQuery[(Timestamp,T)](query, queryExecutor.getTransformerForObservations[T])
   }
@@ -163,8 +189,31 @@ object EquityQueries {
   def getAllValue1AttribPerDayTillDateNoCorrection[T: TypeTag, R](instrumentName: String,
        attributeName: String, tillDate: String, queryExecutor: QueryExecutor[R]):
   Iterator[(Timestamp, T)]  =  {
+    val ts = {
+      try {
+        Timestamp.valueOf(tillDate).getTime
+      }catch {
+        case e: Exception => q5_6JustDateFormatter.parse(tillDate).getTime
+      }
+    }
+    val cal = Calendar.getInstance()
+    cal.setTimeInMillis(ts)
+    val hr = cal.get(Calendar.HOUR_OF_DAY)
+    val min = cal.get(Calendar.MINUTE)
+    val sec = cal.get(Calendar.SECOND)
+    val modDate = if(hr == 0 && min == 1 && sec == 0) {
+      //add time till end of day
+      cal.add(Calendar.HOUR_OF_DAY, 23)
+      cal.add(Calendar.MINUTE, 58)
+      cal.add(Calendar.SECOND, 59)
+      cal.get(Calendar.YEAR) +"-" + (cal.get(Calendar.MONTH) + 1) +"-" + cal.get(Calendar
+        .DAY_OF_MONTH) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) +
+        ":" + cal.get(Calendar.SECOND)
+    }else {
+      tillDate
+    }
     val valType = AttributeCache.getValType(attributeName)
-    val query = query6.format(instrumentName, valType, tillDate)
+    val query = query6.format(instrumentName, valType, modDate)
     println("equity query6 = " + query)
     queryExecutor.executeQuery[(Timestamp,T)](query, queryExecutor.getTransformerForObservations[T])
   }
--- src/test/scala/com/msci/RunApp.scala
+++ src/test/scala/com/msci/RunApp.scala
@@ -1,5 +1,6 @@
 package com.msci
 
+import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
@@ -49,6 +50,11 @@ object RunApp {
 
     this.testQuery1(snc, queryExecutor, equityInstrumentName1)
     this.testQuery2(snc, queryExecutor, equityInstrumentName1)
+    this.testQuery3(snc, queryExecutor, equityInstrumentName1)
+    this.testQuery4(snc, queryExecutor, equityInstrumentName1)
+    this.testQuery5(snc, queryExecutor, equityInstrumentName1)
+    this.testQuery6(snc, queryExecutor, equityInstrumentName1)
+    this.testQuery7(snc, queryExecutor, equityInstrumentName1)
   }
 
   def testQuery1(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
@@ -68,11 +74,166 @@ object RunApp {
 
   def testQuery2(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
                  instrumentName: String): Unit = {
+    val testRs = EquityQueries.get1Value1AttribPerDayLastTimestampBeforeCutOff[Int, DataFrame](
+      instrumentName, "price", "02:00", queryExecutor)
+
+    val (obstime, value) = testRs.next()
+    assert(!testRs.hasNext)
+    println("query obs time = " + obstime)
+    println("query value = " + value)
+    val expectedObsTime = java.sql.Timestamp.valueOf("2016-01-02 02:00:00.0")
+    val expectedValue = 200
+    assert(obstime == expectedObsTime)
+    assert(expectedValue == value)
+  }
+
+  def testQuery3(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
+                 instrumentName: String): Unit = {
+    val q = s"select name from ${Constants.BRF_CON_INST} as x where x.ID = 9"
+    val equityInstrumentName = queryExecutor.executeQuery[String](q, df => {
+      df.collect().map(row => row.getString(0)).toIterator
+    }).next()
+    val testRs = EquityQueries.get1Value1AttribPerDayLastTimestampForYear[Int, DataFrame](
+      equityInstrumentName, "price", 2013, queryExecutor)
+    // only one attribute per day
+    val mapping  = scala.collection.mutable.Map[Int, Set[Int]]()
+    assert(testRs.hasNext)
+    val cal = Calendar.getInstance()
+    while(testRs.hasNext) {
+      val (obstime, value) = testRs.next()
+      cal.setTimeInMillis(obstime.getTime)
+      assert(cal.get(Calendar.YEAR) == 2013)
+      val month = cal.get(Calendar.MONTH)
+      val day = cal.get(Calendar.DAY_OF_MONTH)
+      val doesNotContain = !mapping.contains(month) || !mapping.get(month).contains(day)
+
+      assert(doesNotContain)
+      var set = mapping.getOrElse(month, Set.empty)
+      set += day
+      mapping.put(month, set)
+    }
+  }
+
+
+  def testQuery4(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
+                 instrumentName: String): Unit = {
+
+    val testRs = EquityQueries.getAllValue1AttribPerDay[Int, DataFrame](
+      instrumentName, "price", queryExecutor)
+    val(obs1, val1) = testRs.next()
+    val(obs2, val2) = testRs.next()
+    val(obs3, val3) = testRs.next()
+    val(obs4, val4) = testRs.next()
+    val(obs5, val5) = testRs.next()
+    assert(!testRs.hasNext)
+
+    assert(obs1 == Timestamp.valueOf("2016-01-02 02:00:00.0"))
+    assert(val1 == 200)
+
+    assert(obs2 == Timestamp.valueOf("2016-01-02 04:00:00.0"))
+    assert(val2 == 124)
+
+    assert(obs3 == Timestamp.valueOf("2016-01-02 06:00:00.0"))
+    assert(val3 == 125)
+
+    assert(obs4 == Timestamp.valueOf("2016-01-02 08:00:00.0"))
+    assert(val4 == 126)
+
+    assert(obs5 == Timestamp.valueOf("2016-01-02 10:00:00.0"))
+    assert(val5 == 127)
+
+  }
+
+  def testQuery5(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
+                 instrumentName: String): Unit = {
+
+
+    val q = s"select name from ${Constants.BRF_CON_INST} as x where x.ID = 9"
+    val equityInstrumentName = queryExecutor.executeQuery[String](q, df => {
+      df.collect().map(row => row.getString(0)).toIterator
+    }).next()
+    val timeTill = "2015-08-03 20:00:00"
+    val testRs = EquityQueries.get1Value1AttribPerDayLastTimestampTillDate[Int, DataFrame](
+      equityInstrumentName, "price", timeTill , queryExecutor)
+    val time = Timestamp.valueOf(timeTill)
+    val mapping  = scala.collection.mutable.Map[Int, Set[Int]]()
+    var currentYear = -1
+    assert(testRs.hasNext)
+    val cal = Calendar.getInstance()
+    cal.setTimeInMillis(time.getTime)
+    while(testRs.hasNext) {
+      val (obstime, value) = testRs.next()
+      cal.setTimeInMillis(obstime.getTime)
+      assert(obstime.before(time))
+      if(currentYear != cal.get(Calendar.YEAR)) {
+        mapping.clear()
+        currentYear = cal.get(Calendar.YEAR)
+      }
+      val month = cal.get(Calendar.MONTH)
+      val day = cal.get(Calendar.DAY_OF_MONTH)
+      val doesNotContain = !mapping.contains(month) || !mapping.get(month).contains(day)
+
+      assert(doesNotContain)
+      var set = mapping.getOrElse(month, Set.empty)
+      set += day
+      mapping.put(month, set)
+    }
+
+    val testRs1 = EquityQueries.get1Value1AttribPerDayLastTimestampTillDate[Int, DataFrame](
+      instrumentName, "price", "2016-01-02 03:00:00.0", queryExecutor)
+
+    val (obstime, value) = testRs1.next()
+    assert(!testRs1.hasNext)
+    assert(value == 200)
+    assert(obstime == Timestamp.valueOf("2016-01-02 02:00:00.0"))
+
+    val testRs2 = EquityQueries.get1Value1AttribPerDayLastTimestampTillDate[Int, DataFrame](
+      instrumentName, "price", "2016-01-02", queryExecutor)
+
+    val (obstime2, value2) = testRs2.next()
+    assert(!testRs2.hasNext)
+    assert(value2 == 127)
+    assert(obstime2 == Timestamp.valueOf("2016-01-02 10:00:00.0"))
+  }
+
+  def testQuery6(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
+                 instrumentName: String): Unit = {
+  val testRs1 = EquityQueries.getAllValue1AttribPerDayTillDateNoCorrection[Int, DataFrame](
+      instrumentName, "price", "2016-01-02 03:00:00.0", queryExecutor)
+
+    val (obstime, value) = testRs1.next()
+    assert(!testRs1.hasNext)
+    assert(value == 123)
+    assert(obstime == Timestamp.valueOf("2016-01-02 02:00:00.0"))
+
+    val testRs2 = EquityQueries.getAllValue1AttribPerDayTillDateNoCorrection[Int, DataFrame](
+      instrumentName, "price", "2016-01-02", queryExecutor)
+
+    val (obstime1, value1) = testRs2.next()
+    val (obstime2, value2) = testRs2.next()
+    val (obstime3, value3) = testRs2.next()
+    val (obstime4, value4) = testRs2.next()
+    val (obstime5, value5) = testRs2.next()
+    assert(!testRs2.hasNext)
+    assert(value1 == 123)
+    assert(obstime1 == Timestamp.valueOf("2016-01-02 02:00:00.0"))
+    assert(value2 == 124)
+    assert(obstime2 == Timestamp.valueOf("2016-01-02 04:00:00.0"))
+    assert(value3 == 125)
+    assert(obstime3 == Timestamp.valueOf("2016-01-02 06:00:00.0"))
+    assert(value4 == 126)
+    assert(obstime4 == Timestamp.valueOf("2016-01-02 08:00:00.0"))
+    assert(value5 == 127)
+    assert(obstime5 == Timestamp.valueOf("2016-01-02 10:00:00.0"))
+  }
+
+  def testQuery7(snc: SnappyContext, queryExecutor: QueryExecutor[DataFrame],
+                 instrumentName: String): Unit = {
 
 
     val testRs = EquityQueries.getAllCorrectionsValue1AttribPerDay[Int, DataFrame](
       instrumentName, "price", queryExecutor)
-    
+
     val (obstime, value) = testRs.next()
     assert(!testRs.hasNext)
     println("query obs time = " + obstime)
